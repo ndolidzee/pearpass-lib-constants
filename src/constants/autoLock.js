cfc8c67c23926b3d2f1cc33cdf67ddb7007b396c
Nikoloz Dolidze <n.dolidze@noxtton.com>
2026-06-23 13:49:15 +0400

    Update auto lock timeout options (#44)
    * Update auto lock timeout options
    
    * Restore removed options and add  deprecated property

--- src/constants/autoLock.js
+++ src/constants/autoLock.js
@@ -1,15 +1,26 @@
 import { MS_PER_SECOND, MS_PER_MINUTE, MS_PER_HOUR } from './time.js'
 
 export const AUTO_LOCK_TIMEOUT_OPTIONS = {
-  SECONDS_30: { label: '30 seconds', value: 30 * MS_PER_SECOND },
-  MINUTES_1: { label: '1 minute', value: MS_PER_MINUTE },
-  MINUTES_5: { label: '5 minutes', value: 5 * MS_PER_MINUTE },
-  MINUTES_15: { label: '15 minutes', value: 15 * MS_PER_MINUTE },
-  MINUTES_30: { label: '30 minutes', value: 30 * MS_PER_MINUTE },
-  HOURS_1: { label: '1 hour', value: MS_PER_HOUR },
-  HOURS_4: { label: '4 hours', value: 4 * MS_PER_HOUR },
+  SECONDS_30: {
+    label: '30 seconds',
+    value: 30 * MS_PER_SECOND,
+    deprecated: true
+  },
+  MINUTES_1: { label: '1 Minute', value: MS_PER_MINUTE },
+  MINUTES_3: { label: '3 Minutes', value: 3 * MS_PER_MINUTE },
+  MINUTES_5: { label: '5 Minutes', value: 5 * MS_PER_MINUTE },
+  MINUTES_10: { label: '10 Minutes', value: 10 * MS_PER_MINUTE },
+  MINUTES_15: {
+    label: '15 minutes',
+    value: 15 * MS_PER_MINUTE,
+    deprecated: true
+  },
+  MINUTES_30: { label: '30 Minutes', value: 30 * MS_PER_MINUTE },
+  HOURS_1: { label: '1 Hour', value: MS_PER_HOUR },
+  HOURS_3: { label: '3 Hours', value: 3 * MS_PER_HOUR },
+  HOURS_4: { label: '4 hours', value: 4 * MS_PER_HOUR, deprecated: true },
   NEVER: { label: 'Never', value: null }
 }
 
 export const DEFAULT_AUTO_LOCK_TIMEOUT =
-  AUTO_LOCK_TIMEOUT_OPTIONS.SECONDS_30.value
+  AUTO_LOCK_TIMEOUT_OPTIONS.MINUTES_1.value
